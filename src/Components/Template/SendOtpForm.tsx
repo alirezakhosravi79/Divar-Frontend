e6b8ea710909feeb1333a66205832da9cfbb1d85
alirezass <alirezakhosraviss1526@gmail.com>
2025-09-14 00:44:00 +0330

    base of form step1

--- src/Components/Template/SendOtpForm.tsx
+++ src/Components/Template/SendOtpForm.tsx
@@ -1,8 +1,26 @@
-
-function SendOtpForm() {
-  return (
-    <div>SendOtpForm</div>
-  )
+interface SendOtpFormProps {
+  mobile: string;
+  setMobile: React.Dispatch<React.SetStateAction<string>>;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default SendOtpForm
+const SendOtpForm: React.FC<SendOtpFormProps> = ({ mobile, setMobile, setStep }) => {
+    
+const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+};
+
+  return (
+    <form className="font-vazir" onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>برای استفاده از سایت دیوار، لطفا شماره موبایل خود را وارد کنید. کد تایید
+        به این شماره ارسال خواهد شد.</span>
+        <label htmlFor="input">شماره موبایل خود را وارد کنید.</label>
+        <input type="text" id="input" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)}/>
+        <button className="bg-red-700 text-white" type="submit">ارسال کد تایید</button>
+    </form>
+  );
+};
+
+export default SendOtpForm;
--- src/Pages/AuthPage.tsx
+++ src/Pages/AuthPage.tsx
@@ -4,14 +4,14 @@ import SendOtpForm from "../Components/Template/SendOtpForm"
 
 function AuthPage() {
     //state for forms 1,2
-    const [step , setStep] = useState(1);
+    const [step , setStep] = useState<number>(1);
     //state for mobile
-    const [mobile , setMobile] = useState("");
+    const [mobile , setMobile] = useState<string>("");
     //state for code otp
-    const [code , setCode] = useState("");
+    const [code , setCode] = useState<string>("");
   return (
     <div>
-        {step === 1 && <SendOtpForm />}
+        {step === 1 && <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile}/>}
         {step === 2 && <CheckOtpForm />}
     </div>
   )
